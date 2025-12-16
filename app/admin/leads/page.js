d007f70389f7595ba97e8f9e73d6aec7bf4825cd
Digitalsolution360-Global <globalweb3600@gmail.com>
2025-12-16 10:06:29 +0530

    Change follow_up_date to an empty string if null

--- app/admin/leads/page.js
+++ app/admin/leads/page.js
@@ -1,4 +1,4 @@
-i'use client';
+'use client';
 import dynamic from 'next/dynamic'; 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
@@ -169,7 +169,7 @@ export default function AdminLeads() {
         body: JSON.stringify({ 
           lead_id: selectedLeadForStatus.id,
           remarks: statusUpdate,
-          follow_up_date: followUpDate || null
+          follow_up_date: followUpDate || ''
         })
       });
 
