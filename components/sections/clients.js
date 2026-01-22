1786c17ce5252b9dbec6546f3417c398f387d492
Digitalsolution360-Global <globalweb3600@gmail.com>
2026-01-22 15:35:29 +0530

    Add overflow-hidden class to scrolling container

--- components/sections/clients.js
+++ components/sections/clients.js
@@ -110,7 +110,7 @@ function Clients() {
       </div>
 
       {/* Infinite Scrolling Container */}
-      <div className='relative'>
+      <div className='relative overflow-hidden'>
         {/* Gradient overlays for fade effect */}
         <div className='absolute left-0 top-0 bottom-0 w-32 bg-gradient-to-r from-white to-transparent z-10 pointer-events-none'></div>
         <div className='absolute right-0 top-0 bottom-0 w-32 bg-gradient-to-l from-white to-transparent z-10 pointer-events-none'></div>
